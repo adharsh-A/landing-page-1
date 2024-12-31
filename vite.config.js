fbb677b1a041b0f6ad4c707cecad0b9ad5786744
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-12-31 15:21:32 +0530

    working

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig({
   // Configure external dependencies that are loaded via CDN
   build: {
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: [],
     },
   },
   optimizeDeps: {
-    include: ["react", "react-dom"],
+    include: [],
   },
   css: {
     postcss: "./postcss.config.js",
